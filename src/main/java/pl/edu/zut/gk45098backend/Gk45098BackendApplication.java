7e137a4a695dcfa4a8c209e889010b063e9a6e5c
Krzysiek <szy162@gmail.com>
2022-01-01 16:52:59 +0100

    first commit

---
+++ src/main/java/pl/edu/zut/gk45098backend/Gk45098BackendApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.zut.gk45098backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gk45098BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gk45098BackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/edu/zut/gk45098backend/controller/HelloController.java
@@ -0,0 +1,14 @@
+package pl.edu.zut.gk45098backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello world!";
+    }
+
+}
